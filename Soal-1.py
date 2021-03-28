181543d417a156e5ae575736c35dd19d82219c5a
Humam S Dawa <63929671+humamdawa@users.noreply.github.com>
2021-03-28 13:09:10 +0700

    Add files via upload

---
+++ Soal-1.py
@@ -0,0 +1,6 @@
+nama = input("Nama:")
+umur = input("Umur:")
+tinggi = input("Tinggi badan (cm):")
+
+
+print("Nama saya adalah "+str(nama)+",", "umur saya "+int(umur)+" tahun dan", "tinggi saya "+float(tinggi)+" cm.")
---
+++ Soal-2.py
@@ -0,0 +1,7 @@
+#pi = 3.14159265359
+
+import numpy as np
+radius = input("panjang radius (cm):")
+luas = np.pi*radius**2
+
+print("Luas lingkaran dengan jari-jari "+radius+" cm adalah"+luas+"cm\u00b2.")
